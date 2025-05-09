2bf06bece09c7082e24d3ea4ea9aba92651b2868
Maria Neuhoff <mneuhoff@sr3-r2-a2k-1-2.intra>
2025-05-09 11:25:38 +0200

    create first dataset and graph with marbel and dbg

---
+++ praktikum/my_marbel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name=marbel               # Job name
+#SBATCH --partition=bcf                # Partition name
+#SBATCH --ntasks=1                      # Run on a single task
+#SBATCH --cpus-per-task=10             # Number of CPU cores per task
+#SBATCH --mem=10gb                     # Total memory limit
+#SBATCH --time=1-00:00:00           
+
+marbel --n-species 10 --n-orthogroups 2000 --library-size 100000 --n-samples 3 3 --seed 23
